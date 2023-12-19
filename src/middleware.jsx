13aa5f697f8257a7a210cdd776d1a5f315379e50
Minhaj-Rabby <minhaj12113@gmail.com>
2023-12-19 20:31:45 +0600

    Middleware to Private route

---
+++ src/middleware.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NextResponse} from 'next/server/';
+
+export const middleware = (request) => {
+ 
+    // if(request.nextUrl.pathname!='/login')
+    return (
+     NextResponse.redirect(new URL('/login', request.url))
+  )
+}
+
+export const config ={
+    matcher:["/about/:path*","/student/:path*"]
+  }
+
