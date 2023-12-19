73d3a81c008ed971a56afea67e5dbc0cbe203736
Minhaj-Rabby <minhaj12113@gmail.com>
2023-12-19 19:57:27 +0600

    Page not found added

---
+++ src/app/login/[...not-found]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginNotFound = () => {
+  return (
+    <div>
+      <h2>Login Not Found</h2>
+    </div>
+  )
+}
+
+export default LoginNotFound
---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+// pages/notfound.js
+'use client'
+import React from 'react';
+
+const NotFoundPage = () => {
+
+
+    return (
+        <div>
+            <h2>This Page is not available</h2>
+            {/* <p>Current URL: {asPath}</p> */}
+        </div>
+    );
+};
+
+export default NotFoundPage;
